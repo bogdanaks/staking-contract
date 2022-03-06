7d2a8ac2c866385a356321d3f653880565b820b7
bogdanaksenov <bogdanaks@bk.ru>
2022-03-07 00:01:32 +0300

    Change contract

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -3,55 +3,55 @@ pragma solidity ^0.8.11;
 
 import "hardhat/console.sol";
 
+import "./RewardToken.sol";
+
 contract Staking {
-    IERC20 public rewardsToken;
+    RewardToken public rewardsToken;
     IERC20 public stakingToken;
 
-    uint256 public rewardPercentage = 100;
-    uint public rewardTime = 60 * 60 * 10; // 10 min
-    uint256 public lastUpdateTime;
+    uint32 public rewardTime = 60 * 10; // 10 min
+    uint256 public rewardPercentage = 100; // 100% every 10min
     uint256 public rewardPerTokenStored;
     uint256 private totalSupply;
 
-    mapping(address => uint256) public userRewardPerTokenPaid;
+    // mapping(address => uint256) public userRewardPerTokenPaid;
     mapping(address => uint256) public rewards;
     mapping(address => uint256) public balances;
-    mapping(address => uint256) private stakingTimes;
+    mapping(address => uint256) private stakeTimes;
 
-    constructor(address _stakingToken, address _rewardToken) {
+    constructor(address _stakingToken) {
         stakingToken = IERC20(_stakingToken);
-        rewardsToken = IERC20(_rewardToken);
+        rewardsToken = new RewardToken();
     }
 
-    function earned(address account) internal view returns (uint256) {
-        if (totalSupply == 0) return 0;
-
-        uint256 balanceStaking = balances[account];
-        return balanceStaking + (balanceStaking / 100 * rewardPercentage * 1e18) / totalSupply;
-    }
-
-    function isGreaterThanRewardTime() internal view returns (bool) {
-        return (block.timestamp - stakingTimes[msg.sender] > rewardTime) ? true : false;
-    }
-
-    modifier updateReward(address account) {
-        lastUpdateTime = block.timestamp;
-
-        uint256 earnAmount = earned(account);
-        rewards[account] = earnAmount;
+    modifier updateReward(address _account) {
+        uint256 earnAmount = earned(_account);
+        rewards[_account] = earnAmount;
         _;
     }
 
+    function earned(address _account) internal view returns (uint256) {
+        if (totalSupply == 0) return 0;
+
+        uint256 balanceStaking = balances[_account];
+        return (balanceStaking / 100 * rewardPercentage * 1e18) / totalSupply;
+    }
+
+    function isGreaterThanRewardTime() internal view returns (bool) {
+        return (block.timestamp - stakeTimes[msg.sender] > rewardTime) ? true : false;
+    }
+
     function stake(uint256 _amount) external updateReward(msg.sender) {
+        stakeTimes[msg.sender] = block.timestamp;
         totalSupply += _amount;
         balances[msg.sender] += _amount;
-        stakingTimes[msg.sender] = block.timestamp;
         stakingToken.transferFrom(msg.sender, address(this), _amount);
     }
 
     function unstake(uint256 _amount) external updateReward(msg.sender) {
-        require(isGreaterThanRewardTime(), "You can claim after available reward time");
+        require(balances[msg.sender] >= _amount, "Balance less than amount");
 
+        stakeTimes[msg.sender] = block.timestamp;
         totalSupply -= _amount;
         balances[msg.sender] -= _amount;
         stakingToken.transfer(msg.sender, _amount);
@@ -60,8 +60,11 @@ contract Staking {
     function claim() external updateReward(msg.sender) {
         require(isGreaterThanRewardTime(), "You can claim after available reward time");
 
+        stakeTimes[msg.sender] = block.timestamp;
+
         uint256 reward = rewards[msg.sender];
         rewards[msg.sender] = 0;
+        rewardsToken.mint(reward);
         rewardsToken.transfer(msg.sender, reward);
     }
 }
