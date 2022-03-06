467d17e6ada8ee3ebc727a8a816ab2239a156ace
bogdanaksenov <bogdanaks@bk.ru>
2022-03-07 02:22:45 +0300

    Update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,11 +2,15 @@ import { ethers } from "hardhat";
 
 async function main() {
   const StakingContract = await ethers.getContractFactory("Staking");
-  const staking = await StakingContract.deploy();
+  const LPTokenContract = await ethers.getContractFactory("LPToken");
+  const lpToken = await LPTokenContract.deploy();
+  const staking = await StakingContract.deploy(lpToken.address);
   await staking.deployed();
   const [owner] = await ethers.getSigners();
 
   console.log("Staking deployed to:", staking.address);
+  console.log("LPToken deployed to:", lpToken.address);
+  console.log("Reward token address is:", await staking.rewardsToken());
   console.log("Owner address is: ", owner.address);
 }
 
