8a9baa38dfa8e08a0f58b6b0e40f3cb8e750271e
bogdanaksenov <bogdanaks@bk.ru>
2022-03-07 09:49:22 +0300

    Update

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,12 +4,14 @@ async function main() {
   const StakingContract = await ethers.getContractFactory("Staking");
   const LPTokenContract = await ethers.getContractFactory("LPToken");
   const lpToken = await LPTokenContract.deploy();
-  const staking = await StakingContract.deploy(lpToken.address);
+  const staking = await StakingContract.deploy(
+    process.env.ERC20_CONTRACT_ADDRESS || lpToken.address
+  );
   await staking.deployed();
   const [owner] = await ethers.getSigners();
 
   console.log("Staking deployed to:", staking.address);
-  console.log("LPToken deployed to:", lpToken.address);
+  console.log("LPToken deployed to:", await staking.stakingToken());
   console.log("Reward token address is:", await staking.rewardsToken());
   console.log("Owner address is: ", owner.address);
 }
